c4df6e5c5e1ad302e2166eb2190a6d401490391c
nfailor <nfailor@gmail.com>
2023-12-11 20:36:34 -0600

    Dropped script.js for server.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const context = { name: 'World' };
-const output = template(context);
-
-console.log(output); // Hello, World!
-
---
+++ server.js
@@ -0,0 +1,4 @@
+const context = { name: 'World' };
+const output = template(context);
+
+console.log(output); // Hello, World!
