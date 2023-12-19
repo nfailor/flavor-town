ee5a66fa8b913c2aa2ee6ffc9a04976563a3a277
srgowen <srgowen2@gmail.com>
2023-12-18 20:50:25 -0500

    added recipes

--- seeds/demo-post.js
+++ seeds/demo-post.js
@@ -6,6 +6,31 @@ const postdata = [
     body: 'This is designed to make sure my routes and posts are properly executing',
     user_id: 1,
   },
+  {
+    title: 'Spicy Chocolate Delight',
+    body: 'Indulge in the rich combination of dark chocolate and chili for a unique taste experience.',
+    user_id: 3,
+  },
+  {
+    title: 'Citrus Burst Smoothie',
+    body: 'Blend together oranges, pineapples, and a hint of mint for a refreshing and nutritious smoothie.',
+    user_id: 4,
+  },
+  {
+    title: 'Savory Pancake Surprise',
+    body: 'Experiment with unexpected ingredients like cheese, spinach, and sun-dried tomatoes for a twist on traditional pancakes.',
+    user_id: 5,
+  },
+  {
+    title: 'Coconut Caramel Dream Bars',
+    body: 'Create a heavenly treat by combining coconut, caramel, and a buttery crust for a delightful dessert.',
+    user_id: 6,
+  },
+  {
+    title: 'Mango Tango Salsa',
+    body: 'Mix ripe mangoes, tomatoes, and jalapeños for a zesty salsa that pairs perfectly with chips or grilled dishes.',
+    user_id: 7,
+  },
 ];
 
 const seedPost = () => Post.bulkCreate(postdata);
--- seeds/demo-user.js
+++ seeds/demo-user.js
@@ -7,6 +7,42 @@ const userdata = [
         username: 'testUser',
         email: 'testUser@gmail.com',
         password: '1234'
+    },
+    {
+        id: 2,
+        username: 'gator',
+        email: 'GatorGonGetEm@gmail.com',
+        password: '2345'
+    },
+    {
+        id: 3,
+        username: 'Crocodile_dundee',
+        email: 'juiceworld2020@gmail.com',
+        password: '2345'
+    },
+    {
+        id: 4,
+        username: 'JungleExplorer',
+        email: 'wildernessdiscover@gmail.com',
+        password: 'nature789'
+    },
+    {
+        id: 5,
+        username: 'UrbanAdventurer',
+        email: 'cityscape@gmail.com',
+        password: 'metropolis123'
+    },
+    {
+        id: 6,
+        username: 'SpacePioneer',
+        email: 'outerspace@gmail.com',
+        password: 'galaxy456'
+    },
+    {
+        id: 7,
+        username: 'MysticTraveler',
+        email: 'enchantedjourney@gmail.com',
+        password: 'magic789'
     }
 ];
 
